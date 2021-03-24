fcfca08b1a3f8a2756f0a4cd736bc60cc8bd4848
Joerg Wunsch <dl8dtl@s09.sax.de>
2021-03-24 10:02:31 +0100

    Update email address

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
- * <dl8dtl@darc.de> wrote this file.  As long as you retain this notice you
+ * <dl8dtl@s09.sax.de> wrote this file.  As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a beer in return.        Joerg Wunsch
  * ----------------------------------------------------------------------------
